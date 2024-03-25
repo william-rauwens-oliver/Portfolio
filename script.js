d83f52be0c24e7729a0f0a261bf624d3dbd87ace
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-03-25 22:35:33 +0100

    add code for volet page hamburger

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.querySelector('.hamburger-menu').addEventListener('click', () => {
-    document.querySelector('.nav-bar').classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+    // Sélection du hamburger menu
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    // Sélection de la nav
+    const sideNav = document.querySelector('.side-nav');
+
+    // Écouteur d'événement pour le clic sur le hamburger menu
+    hamburgerMenu.addEventListener('click', () => {
+        // Basculement de la classe 'open' pour afficher ou masquer la nav
+        sideNav.classList.toggle('open');
+    });
 });
