a1d162bce4220977dd63fe2e67a86468b46637ee
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-03-25 22:47:32 +0100

    delete commentary

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Sélection du hamburger menu
     const hamburgerMenu = document.querySelector('.hamburger-menu');
-    // Sélection de la nav
     const sideNav = document.querySelector('.side-nav');
-
-    // Écouteur d'événement pour le clic sur le hamburger menu
     hamburgerMenu.addEventListener('click', () => {
-        // Basculement de la classe 'open' pour afficher ou masquer la nav
         sideNav.classList.toggle('open');
     });
 });
